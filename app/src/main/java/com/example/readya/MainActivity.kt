f9a9ab0fa7ed759811c5c6208a05d26d41493221
JohnMV <johnmarulanda74@gmail.com>
2024-06-12 17:36:31 -0500

    Inicializacion de proyecto y coneccion Firebase

---
+++ app/src/main/java/com/example/readya/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.readya
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.readya.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+
+        binding.loginBtn.setOnClickListener {
+
+
+        }
+
+
+        binding.skipBtn.setOnClickListener {
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/readya/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.readya
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+            startActivity(Intent(this, MainActivity::class.java))
+        Handler().postDelayed(Runnable {  }, 2000) //2 Segundos de delay
+    }
+}
